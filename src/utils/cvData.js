587c01309eee4c883e3d81a08dfe33e79a45e71f
Kiera <kiera.chelsea3@gmail.com>
2022-01-11 12:00:53 +0000

    type capitalisation

--- src/utils/cvData.js
+++ src/utils/cvData.js
@@ -39,7 +39,7 @@ export default {
       icon: <TwitterIcon />,
     },
   },
-  about: `  ● Self-taught Front-End Developer with proven skills in React and Javascript. Coming from a background in digital product creation, scripting and 3D modelling.
+  about: `  Self-taught Front-End Developer with proven skills in React and Javascript. Coming from a background in digital product creation, scripting and 3D modelling.
 `,
 
   experiences: [
@@ -48,39 +48,39 @@ export default {
 
       date: "2021/05 - (ongoing)",
 
-      description: `● A fully functioning React-based Shopify e-commerce site for a Sheffield client. Uses the Shopify API, Shopify-Buy and EmailJS.        
-        ● Only front-end needed as EmailJS and React-Hook-Form handle the contact form. 
-        ● Products saved to local storage through Shopify-Buy. Chakra UI, Material UI used for styling.`,
+      description: `A fully functioning React-based Shopify e-commerce site for a Sheffield client. Uses the Shopify API, Shopify-Buy and EmailJS.        
+        Only front-end needed as EmailJS and React-Hook-Form handle the contact form. 
+        Products saved to local storage through Shopify-Buy. Chakra UI, Material UI used for styling.`,
     },
 
     {
       title: "FaceDetect, Face detection Web application ",
       date: "2021/07 - 2021/09",
-      description: `● React-based full stack personal project. 
-        ● The user can create an account or log in as a guest. 
-        ● Uses Node.Js with Express.Js for the server and  PostgreSQL for the database. 
-        ● Makes external API calls to Clarifai's Face Detection Model.`,
+      description: `React-based full stack personal project. 
+        The user can create an account or log in as a guest. 
+        Uses Node.Js with Express.Js for the server and  PostgreSQL for the database. 
+        Makes external API calls to Clarifai's Face Detection Model.`,
     },
     {
       title: "CV as a Webpage",
       date: "2021/11",
 
-      description: `● React-based project hosted on Heroku.     
-        ● Uses Bootstrap and Material UI for styling.  
+      description: `React-based project hosted on Heroku.     
+        Uses Bootstrap and Material UI for styling.  
        `,
     },
 
     {
       title: "Architectural Visualiser - Miller Hare",
       date: "2016/10 - Present | London, UK",
-      description: `● Used javascript to speed up a very repetitive in-house 3D model texturing process by 70%.  
-        ● Optimised and automated old company workflows by learning Max Script (the scripting language for 3ds Max).`,
+      description: `Used Javascript to speed up a very repetitive in-house 3D model texturing process by 40%.  
+        Optimised and automated old company workflows by learning Max Script (the scripting language for 3ds Max).`,
     },
 
     {
       title: "Interactive Unreal Engine Designer - Miller Hare",
       date: "2016/10 - 2018/10 | London, UK",
-      description: `● Worked with C++ based Unreal Engine to represent architectural projects in Virtual Reality. `,
+      description: `Worked with C++ based Unreal Engine to represent architectural projects in Virtual Reality. `,
     },
   ],
 
@@ -169,25 +169,25 @@ export default {
   softSkills: [
     {
       title: "Communication",
-      description: `● Succesfully persuaded managers to accept my workflow improvements. 
-      ● Taught these new methods to other teams by explaining them in emails or presenting them verbally.
-      ● Explained technical subjects in simple terms in order to share ideas with non-technical colleagues.
+      description: `Succesfully persuaded managers to accept my workflow improvements. 
+      Taught these new methods to other teams by explaining them in emails or presenting them verbally.
+      Explained technical subjects in simple terms in order to share ideas with non-technical colleagues.
       `,
     },
     {
       title: "Collaboration",
-      description: `● Have helped set up in-house organisational tools that divide workload, keep track of timeframes and implement version control.
-      ● Mentored new team members in the use of bespoke software and the companies' best practices.`,
+      description: `Have helped set up in-house organisational tools that divide workload, keep track of timeframes and implement version control.
+      Mentored new team members in the use of bespoke software and the companies' best practices.`,
     },
     {
       title: "Analytical Abilities",
-      description: `● Have successfuly identified company workflow trends over time and found the key performance indicators, in our case 3D visulisation image turnover. Realised that by reducing the preparatory work time, we can deliver images to clients 40% faster. 
-      ● Created Photoshop templates for my team to speed up image preparation phase. Wrote several scripts to automate Photoshop tasks.`,
+      description: `Have successfuly identified company workflow trends over time and found the key performance indicators, in our case 3D visulisation image turnover. Realised that by reducing the preparatory work time, we can deliver images to clients 40% faster. 
+      Created Photoshop templates for my team to speed up image preparation phase. Wrote several scripts to automate Photoshop tasks.`,
     },
     {
       title: "Self-Learner",
-      description: `● The drive to find solutions has lead me down the route of learning new technologies and also new ways of using the existing ones. 
-      ● Avidly following the evolution of developer practices and tools in order to learn and implement them in my work.`,
+      description: `The drive to find solutions has lead me down the route of learning new technologies and also new ways of using the existing ones. 
+      Avidly following the evolution of developer practices and tools in order to learn and implement them in my work.`,
     },
   ],
 };
